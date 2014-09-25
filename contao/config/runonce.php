841d496b9b4fae4240121a6741f631415a4c5b9e
kozi <martin@kozianka.de>
2014-09-25 08:21:34 +0200

    Updated copyright info

--- contao/config/runonce.php
+++ contao/config/runonce.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ *
+ * PHP version 5
+ * @copyright  Martin Kozianka 2013-2014 <http://kozianka.de/>
+ * @author     Martin Kozianka <http://kozianka.de/>
+ * @package    additional_metafields
+ * @license    LGPL
+ * @filesource
+ */
+
 use \AdditionalMetafields\MetafieldsModel;
 
 class MetafieldsRunonceJob extends \Controller {
