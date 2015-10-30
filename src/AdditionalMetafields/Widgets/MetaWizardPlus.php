3264f228fc91ae402bddfeeeea6889b7cfb71de3
Martin Kozianka <martin@kozianka.de>
2015-10-30 11:59:25 +0100

    Added CSS style for new method

--- src/AdditionalMetafields/Widgets/MetaWizardPlus.php
+++ src/AdditionalMetafields/Widgets/MetaWizardPlus.php
@@ -1,17 +1,17 @@
 <?php namespace AdditionalMetafields\Widgets;
 
-/**
- * Contao Open Source CMS
- * Copyright (C) 2005-2015 Leo Feyer
- *
- *
- * PHP version 5
- * @copyright  Martin Kozianka 2013-2015 <http://kozianka.de/>
- * @author     Martin Kozianka <http://kozianka.de/>
- * @package    contao-metafields
- * @license    LGPL
- * @filesource
- */
+    /**
+     * Contao Open Source CMS
+     * Copyright (C) 2005-2015 Leo Feyer
+     *
+     *
+     * PHP version 5
+     * @copyright  Martin Kozianka 2013-2015 <http://kozianka.de/>
+     * @author     Martin Kozianka <http://kozianka.de/>
+     * @package    contao-metafields
+     * @license    LGPL
+     * @filesource
+     */
 
 /**
  * Class MetaWizardPlus
@@ -35,18 +35,24 @@ class MetaWizardPlus extends MetaWizard
     {
         $helper            = MetafieldsHelper::getInstance();
         $arrMetaFieldsPlus = $helper->getFields($this->activeRecord);
-
+        
         if ($arrMetaFieldsPlus !== null)
         {
+            $cssStyle  = '';
+            $tmplStyle = ".tl_metawizard label[for^=ctrl_%s] { font-weight:bold; }\n";
             $arrMetaFields = $this->metaFields;
             foreach ($arrMetaFieldsPlus as $field => $label)
             {
                 $arrMetaFields[] = $field;
 
                 // TODO Translation
-                $GLOBALS['TL_LANG']['MSC']['aw_' . $field] = $label;
+                $GLOBALS['TL_LANG']['MSC']['aw_'.$field] = $label;
+
+                // Add CSS style
+                $cssStyle .= sprintf($tmplStyle, $field);
             }
-            $this->metaFields = $arrMetaFields;
+            $this->metaFields         = $arrMetaFields;
+            $GLOBALS['TL_MOOTOOLS'][] = "\n\n<style>\n".$cssStyle."</style>\n\n";
         }
         return parent::generate();
     }
