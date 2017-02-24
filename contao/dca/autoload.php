71dd257e364e18e7dd27702cbdb001c084facacb
kozi <kozi@users.noreply.github.com>
2017-02-24 15:59:51 +0100

    Added autoload.php

---
+++ contao/dca/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+ClassLoader::addClasses(array(
+    'Metafields\\MetafieldsHelper'          => 'system/modules/metafields/src/Metafields/MetafieldsHelper.php',
+    'Metafields\\Widgets\\MetaWizardPlus'   => 'system/modules/metafields/src/Metafields/Widgets/MetaWizardPlus.php',
+    'Metafields\\Models\\MetafieldsModel'   => 'system/modules/metafields/src/Metafields/Models/MetafieldsModel.php',
+));
